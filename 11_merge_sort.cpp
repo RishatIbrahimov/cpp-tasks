19067c3b93462dd92d60f7563a9adad867cb29be
RishatIbrahimov <rishat.ibrahimov@cinarra.com>
2016-12-16 22:10:35 +0300

    merge sort

---
+++ 11_merge_sort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+void merge(int *begin, int *m, int *end, int *buffer) {
+	int len = end - begin;
+	int lenA = m - begin;
+	int lenB = end - m;
+	int i = 0, j = 0, k = 0;
+	while (i < lenA && j - lenB) {
+		if (begin[i] < m[j]) {
+			buffer[k++] = begin[i++];
+		} else {
+			buffer[k++] = m[j++];
+		}
+	}
+	while (i < lenA) {
+		buffer[k++] = begin[i++];
+	}
+	while (j < lenB) {
+		buffer[k++] = m[j++];
+	}
+	for (int l = 0; l < len; l++) {
+		begin[l] = buffer[l];
+	}
+}
+
+
+void merge_sort(int *begin, int *end, int *buffer) {
+	if (end - begin > 1) {
+		int *m = begin + (end - begin) / 2;
+		merge_sort(begin, m, buffer);
+		merge_sort(m, end, buffer);
+		merge(begin, m, end, buffer);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int a[100000];
+	int c[100000];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	merge_sort(a, a + n, c);
+
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
